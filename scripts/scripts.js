28e40db5eb830d20fe6be42fb431b28b29e3c641
Alexander <alexander.shin87@gmail.com>
2019-03-18 11:20:26 -0400

    cleaned up and added hamburger menu

---
+++ scripts/scripts.js
@@ -0,0 +1,11 @@
+
+$(function(){
+	
+	$('.hamburger').on('click', function () {
+		$('.hamburger').toggleClass('is-active');
+	});
+
+	$('.hamburger').on('click', function(){
+		$('.topNav ul').toggleClass('active');
+	});
+});
